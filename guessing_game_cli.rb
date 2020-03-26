29c85eb1e45379cab64677bbd05144aaa5d7855b
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-26 00:15:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,10 @@ def num
 end
 
 def prompt
-  puts "Oh heddo wady, it's ya boi Phteven! Im"
+  puts "Hi there! "I'm thinking of a number between 1 and 6..." "Can you guess what the number is? Enter it below!"
+  puts
+  puts
+end
   
 
 def run_guessing_game
