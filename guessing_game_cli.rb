83b769a800ecb7b978203ba11012a47d47054f16
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-26 00:17:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,9 +9,13 @@ def prompt
   puts "Hi there!" 
   sleep 0.5
   puts "I'm thinking of a number between 1 and 6..."
+  sleep 0.5
   puts "Can you guess what the number is?"
+  sleep 0.5
   puts "Enter it below!"
 end
+  
+def capture_guess
   
 
 def run_guessing_game
